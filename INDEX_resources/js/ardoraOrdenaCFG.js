e60239f88455ccce93afaad760c593920365a78c
0822-Ka <alfaesmirnainfanteo8@gmail.com>
2025-07-07 17:07:08 +0200

    Add files via upload

---
+++ INDEX_resources/js/ardoraOrdenaCFG.js
@@ -0,0 +1,30 @@
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=5; attempts=0; attemptsMax=1;
+var score=0; scoreMax=5; scoreInc=1; scoreDec=0
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk=""; messageTime=""; messageError=""; messageErrorG=""; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="SU5ERVg="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["WW8gdXNvIFBhZGxldA==","SmFtYm9hcmQgZXMgZGl2ZXJ0aWRv","RXNjcmlibyB1bmEgbm90YQ==","RWRpdGFtb3MgZWwgbXVybw==","Q29sYWJvcmFtb3MgZW4gbMOtbmVh"];imaW=["Captura_de_pantalla_7-7-2025_165621_st3.depositphotos.com.jpeg","Captura_de_pantalla_7-7-2025_165621_st3.depositphotos.com.jpeg","Captura_de_pantalla_7-7-2025_165621_st3.depositphotos.com.jpeg","Captura_de_pantalla_7-7-2025_165621_st3.depositphotos.com.jpeg","Captura.JPG"];queW=["USAR","DIVERTIRSE","ESCRIBIR","EDITAR","TRABAJAR JUNTOS"];altW=["","","","",""];c=[13,21,16,16,20];
+var infoW=["","","","",""];
+var auW=["","","","",""];
+var actMaxWidth="600"; actMaxHeight="300";indexG=0;profG=0;dirMedia="INDEX_resources/media/"; textBNext="";
+var wordsG=[];imageW=[];questionW=[];audioW=[];cG=[];alterW=[];informaW=[];
